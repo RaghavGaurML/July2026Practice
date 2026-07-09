efe1ba271edac16ab78887beb69b0828e865b7d1
Raghav Gaur <raghavgaurrg@gmail.com>
2026-07-08 17:49:55 -0700

    Changed camel cased variables to snake case

--- core_data_structures.py
+++ core_data_structures.py
@@ -2,27 +2,27 @@ users = [{"id": 1, "name": "Asha", "age": 25, "active": True},
          {"id": 2, "name": "Bob", "age": 30, "active": False}, 
          {"id": 3, "name": "Cathy", "age": 41, "active": True}]
 
-activeUserNames = [u["name"] for u in users if u["active"] == True]
-under35UserNames = [u["name"] for u in users if u["age"] <= 35]
+active_user_names = [u["name"] for u in users if u["active"]]
+under_35_user_names = [u["name"] for u in users if u["age"] <= 35]
 
-under35AndActiveUserNamesI = set(activeUserNames).intersection(set(under35UserNames))
-under35AndActiveUserNamesII = [u["name"] for u in users if u["age"] <= 35 and u["active"] == True]
+under_35_and_active_user_names_I = set(active_user_names).intersection(set(under_35_user_names))
+under_35_and_active_user_names_II = [u["name"] for u in users if u["age"] <= 35 and u["active"] == True]
 
-print(activeUserNames)
-print(under35UserNames)
-print(under35AndActiveUserNamesI)
-print(under35AndActiveUserNamesII)
+print(active_user_names)
+print(under_35_user_names)
+print(under_35_and_active_user_names_I)
+print(under_35_and_active_user_names_II)
 
-firstUserName = users[0].get("name")
-firstUserProfession = users[0].get("profession", None)
+first_user_name = users[0].get("name")
+first_user_profession = users[0].get("profession", None)
 
-print(firstUserName)
-print(firstUserProfession)
+print(first_user_name)
+print(first_user_profession)
 
-firstUserInfo = users[0].items()
-firstUserKeys = users[0].keys()
-firstUserValues = users[0].values()
+first_user_info = users[0].items()
+first_user_keys = users[0].keys()
+first_user_values = users[0].values()
 
-print(firstUserInfo)
-print(firstUserKeys)
-print(firstUserValues)
+print(first_user_info)
+print(first_user_keys)
+print(first_user_values)
