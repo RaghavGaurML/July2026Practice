844e42c757b9488c4ba319bb2b723e5f16034836
Raghav Gaur <raghavgaurrg@gmail.com>
2026-07-08 17:30:45 -0700

    Initial commit, simple list and dictionary work

---
+++ practice_7_8_26.py
@@ -0,0 +1,15 @@
+users = [{"id": 1, "name": "Asha", "age": 25, "active": True}, 
+         {"id": 2, "name": "Bob", "age": 30, "active": False}, 
+         {"id": 3, "name": "Cathy", "age": 41, "active": True}]
+
+activeUserNames = [u["name"] for u in users if u["active"] == True]
+under35UserNames = [u["name"] for u in users if u["age"] <= 35]
+
+under35AndActiveUserNamesI = set(activeUserNames).intersection(set(under35UserNames))
+under35AndActiveUserNamesII = [u["name"] for u in users if u["age"] <= 35 and u["active"] == True]
+
+
+print(activeUserNames)
+print(under35UserNames)
+print(under35AndActiveUserNamesI)
+print(under35AndActiveUserNamesII)
