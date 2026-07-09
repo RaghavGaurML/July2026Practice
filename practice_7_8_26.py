1946ef9603b068e05295f4505410f73292f41d44
Raghav Gaur <raghavgaurrg@gmail.com>
2026-07-08 17:41:44 -0700

    Additional dictionary maniputlation and playing with get

--- practice_7_8_26.py
+++ practice_7_8_26.py
@@ -8,8 +8,21 @@ under35UserNames = [u["name"] for u in users if u["age"] <= 35]
 under35AndActiveUserNamesI = set(activeUserNames).intersection(set(under35UserNames))
 under35AndActiveUserNamesII = [u["name"] for u in users if u["age"] <= 35 and u["active"] == True]
 
-
 print(activeUserNames)
 print(under35UserNames)
 print(under35AndActiveUserNamesI)
 print(under35AndActiveUserNamesII)
+
+firstUserName = users[0].get("name")
+firstUserProfession = users[0].get("profession", None)
+
+print(firstUserName)
+print(firstUserProfession)
+
+firstUserInfo = users[0].items()
+firstUserKeys = users[0].keys()
+firstUserValues = users[0].values()
+
+print(firstUserInfo)
+print(firstUserKeys)
+print(firstUserValues)
